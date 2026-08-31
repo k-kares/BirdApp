5997157cda641e7bc62f9a5eb0ad7e7533fbe1ea
karlo <karlo.kares7@gmail.com>
2026-09-01 01:15:53 +0200

    Napravljen web scraper

---
+++ BirdApp/Models/Bird.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdApp.Models
+{
+    internal class Bird
+    {
+        public string ScientificName { get; set; } = "";
+        public string CanonicalName { get; set; } = "";
+        public string Rank { get; set; } = "";
+        public string Kingdom { get; set; } = "";
+        public string Phylum { get; set; } = "";
+        public string Class { get; set; } = "";
+        public string Order { get; set; } = "";
+        public string Family { get; set; } = "";
+        public string Genus { get; set; } = "";
+    }
+}
